9ef5abc5ec903b16e586a30eee31d202c0c3422f
leonchangcamila <camila.iurd12@gmail.com>
2022-05-09 20:14:24 -0300

    desafio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import ItemListContainer from "./components/ItemListContainer";
 import Navbar from "./components/Navbar/Navbar";
+import ItemCount from "./components/ItemCount";
 
 
 const App = () => {
   return (
+
     <Navbar />
+    <ItemCount />
     <ItemListContainer producto="Camperas" precio="5000"/>
 
   );
---
+++ src/components/ItemCount.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState} from 'react';
+import './Counter.css';
+
+const Counter = ({ stock, initial, onAdd }) => {
+    const [number, setNumber] = useState(1);
+
+    const substract = () => {
+        number > initial && setNumber(number - 1);
+    };
+
+    const addToCart = () => {
+        onAdd(number);
+    };
+
+    return (
+        <div className="container-buton">
+            <div className="container-add-substract">
+                <button onClick={add}>+</button>
+                <p>{number}</p>
+                <button onClick={substract}>-</button>
+            </div>
+            <div>
+                <button
+                    disabled={number === 0}
+                    className={number === 0 ? 'disabled' : 'add'}
+                    onClick={addToCart}
+                >
+                    Agregar al carrito
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
