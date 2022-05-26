b59147395403f1c22b2f4e95215d96baa2a46b54
leonchangcamila <camila.iurd12@gmail.com>
2022-05-25 21:45:04 -0300

    desafio

---
+++ src/Context/CartContext.js
@@ -0,0 +1,28 @@
+import { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+function CartContextProvider({children}) {
+    const [cartList, setCartList] = useState([])
+
+        //function agregarAlCarrito(item) {
+        //      setCartList( [...cartList, item] )
+        //}
+    function agregarAlCarrito(item) {
+        setCartList( [...cartList, item] )
+    }
+    function borrarCarrito() {
+        setCartList([])
+    }
+
+    return (
+        <CartContext.Provider value={{
+            cartList,
+            agregarAlCarrito
+        }}>
+            { children }
+        </CartContext.Provider>
+    )
+}
+
+export default CartContextProvider
